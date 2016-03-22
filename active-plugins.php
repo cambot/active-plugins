85bfef21eeb1f818ea47f5d5da74f52d110968e5
Cameron <jcmaci03@gmail.com>
2016-03-22 09:41:05 -0400

    Initial commit.  Fork of https://wordpress.org/plugins/active-plugins-on-multisite/

---
+++ active-plugins.php
@@ -0,0 +1,186 @@
+<?php
+/*
+Plugin Name: Active Plugins
+Plugin URI: http://trepmal.com/plugins/active-plugins-on-multisite/
+Description: Get number of users for each active plugin (minus network-activated). Then break down by site.
+Author: Kailey Lampert
+Version: 1.6
+Author URI: http://kaileylampert.com/
+Network: true
+
+Copyright (C) 2011-12 Kailey Lampert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+class activeplugins {
+
+	function __construct() {
+		add_action( 'init', array( &$this, 'init' ) );
+	}
+
+	function init() {
+		if ( ! is_multisite() )
+			add_action( 'admin_notices', array( &$this, 'admin_notices' ) );
+		else
+			add_action( 'network_admin_menu', array( &$this, 'network_admin_menu' ) );
+	}
+
+	function admin_notices() {
+		echo '<div class="error fade"><p>';
+		_e( 'Acitve Plugins is for multisite use only.', 'active-plugins' );
+		echo '</p></div>';
+	}
+
+	function network_admin_menu() {
+		add_submenu_page( 'settings.php', __( 'Active Plugins', 'active-plugins' ), __( 'Active Plugins', 'active-plugins' ), 'unfiltered_html', __FILE__, array( &$this, 'page' ) );
+	}
+
+	function page() {
+
+		echo '<div class="wrap">';
+		echo '<h2>'. __( 'Active Plugins', 'active-plugins' ) .'</h2>';
+		echo '<p>'. __( 'Network-Activated plugins not listed.', 'active-plugins' ) .'</p>';
+
+		global $wpdb;
+		$query = "SELECT * FROM {$wpdb->blogs}, {$wpdb->registration_log}
+							WHERE site_id = '{$wpdb->siteid}'
+							AND {$wpdb->blogs}.blog_id = {$wpdb->registration_log}.blog_id";
+
+		$blog_list = $wpdb->get_results( $query, ARRAY_A ); //get blogs
+		$all_plugins = get_plugins();
+		$plugins_list = array_keys( get_plugins() );
+
+		$pi = array();
+
+		//add main site to beginning
+		$blog_list[-1] = array( 'blog_id' => 1 );
+		ksort($blog_list);
+		foreach( $blog_list as $k => $info ) {
+			//loop through the blogs
+			//store active plugins is giant array index by blog id
+			$bid = $info['blog_id'];
+			$pi[ $bid ] = get_blog_option( $bid, 'active_plugins' );
+		}
+		$pi = array_filter($pi); //remove empties
+
+		$pi_count = array();
+		foreach($pi as $k => $v_array) {
+			//put all active plugins into one array, we can then count duplicate values
+			$pi_count = array_merge($pi_count, $v_array);
+		}
+
+		echo '<div style="background:#f3f3f3;padding:5px;">';
+		_e( 'Totals (each active plugin and how many users)', 'active-plugins' );
+
+			$totals = $tags = array_count_values( $pi_count );
+			ksort( $totals );
+			echo '<ul class="ul-disc">';
+			foreach( $totals as $name => $tot) {
+
+				if ( strpos( $name, '/') !== false) {
+					$dir = WP_PLUGIN_DIR . '/' . dirname( $name );
+					$dottags = ( glob( $dir . '/*.tag') );
+					if ( ! empty( $dottags ) )
+						$tags[ $name ] = str_replace( $dir . '/', '', str_replace( '.tag', '', $dottags['0'] ) );
+				}
+
+				if ( in_array( $name, $plugins_list ) ) {
+					$plugins_list = array_flip( $plugins_list );
+					unset( $plugins_list[ $name ] );
+					$plugins_list = array_flip( $plugins_list );
+				}
+
+				$version = isset( $all_plugins[$name]['Version'] ) ? $all_plugins[$name]['Version'] : '';
+				$version = sprintf( __( 'v%s', 'active-plugins' ), $version );
+				if ( isset( $all_plugins[ $name ] ) ) {
+					$label = sprintf( __( '%1$s %2$s', 'active-plugins' ), $all_plugins[$name]['Name'], $version );
+				} else {
+					$label = sprintf( __( '%s (Uninstalled)', 'active-plugins' ), $name );
+				}
+
+				$label .= is_numeric( $tags[ $name ] ) ? '' : sprintf( __( ' (tagged: %s)', 'active-plugins' ), $tags[ $name ] );
+
+				$fulllabel = sprintf( _n( '<strong>%s</strong> is used by %d site', '<strong>%s</strong> is used by %d sites', $tot, 'active-plugins' ), $label, $tot );
+				echo "<li>$fulllabel</li>";
+
+			}
+			echo '</ul>';
+
+			//find which are network-activated
+			$network_plugins = array_flip( get_site_option('active_sitewide_plugins') );
+			//remove those from our list
+			$remove_network = array_diff( $plugins_list, $network_plugins );
+
+			//show which not-network-activated plugins have 0 users
+			_e( 'Plugins with zero (0) users:', 'active-plugins' );
+			echo '<ul class="ul-disc">';
+			foreach( $remove_network as $k => $inactive ) {
+				// $realname = $all_plugins[$inactive]['Name'] . ' v' . $all_plugins[ $inactive ]['Version'];
+				$version = isset( $all_plugins[$name]['Version'] ) ? $all_plugins[$name]['Version'] : '';
+				$version = sprintf( __( 'v%s', 'active-plugins' ), $version );
+				$realname = sprintf( __( '%1$s %2$s', 'active-plugins' ), $all_plugins[ $inactive ]['Name'], $version );
+				$unused[] = "<li>{$realname}</li>";
+			}
+			echo empty( $unused ) ? '<li><em>'. __( 'none', 'active-plugins' ) .'</em></li>' : implode( $unused );
+			echo '</ul>';
+
+		echo '</div>';
+
+		echo '<div style="background:#dfdfdf;padding:5px;margin-top:30px;">';
+			foreach( $pi as $siteid => $list ) {
+
+				switch_to_blog( $siteid );
+
+				$edit = network_admin_url( "site-info.php?id=$siteid" );
+				$view = home_url();
+				$dash = admin_url();
+				$plugins = admin_url('/plugins.php');
+
+				$blogname = get_bloginfo('name');
+				$edit_label = __( 'Edit', 'active-plugins' );
+				$view_label = __( 'View', 'active-plugins' );
+				$dashboard_label = __( 'Dashboard', 'active-plugins' );
+				$plugins_label = __( 'Plugins', 'active-plugins' );
+
+				echo "<h3>$blogname ($siteid) [<a href='$edit'>$edit_label</a>] [<a href='$view'>$view_label</a>] [<a href='$dash'>$dashboard_label</a>] [<a href='$plugins'>$plugins_label</a>]</h3>";
+				echo '<ul class="ul-disc">';
+				$tagged = array();
+				$nottagged = array();
+				foreach( $list as $name ) {
+					$realname = isset( $all_plugins[ $name ] ) ? $all_plugins[ $name ]['Name'] : $name;
+					if ( is_numeric( $tags[ $name ] ) )
+						$nottagged[] .= "<li>{$realname}</li>";
+					else
+						$tagged["<li>({$tags[ $name ]}) $realname</li>"] = $tags[ $name ];
+				}
+				asort( $tagged );
+				$tagged = array_keys( $tagged );
+				echo implode( $tagged );
+
+				sort( $nottagged );
+				echo implode( $nottagged );
+				echo '</ul><hr />';
+
+				restore_current_blog();
+			}
+
+		echo '</div>';
+
+		echo '</div>';
+
+	}// end page()
+
+}
+new activeplugins();
---
+++ readme.txt
@@ -0,0 +1,84 @@
+=== Active Plugins ===
+Contributors: trepmal 
+Donate link: http://kaileylampert.com/donate.php
+Tags: mutlisite, plugins, utility
+Requires at least: 3.2.1
+Tested up to: 3.5
+Stable tag: 1.6
+
+Generates a list of plugins that are currently in use
+
+== Description ==
+
+Generates a list of plugins that are currently in use, excluding Network-Activated
+
+Untested with anything older than WP 3.2.1.
+
+If you feel this plugin deserves a low-star rating, please indicate why by starting a thread in the forum. If something is broken, I need you to tell me so I can fix it. Thanks.
+
+* [other plugins by this author](http://wordpress.org/extend/plugins/profile/trepmal)
+* [author's profile](http://profiles.wordpress.org/users/trepmal/)
+* [author's forum profile](http://wordpress.org/support/profile/trepmal)
+* [donate](http://kaileylampert.com/donate/)
+* [author's website](http://kaileylampert.com)
+
+== Installation ==
+
+1. Upload `active-plugins.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Find Active Plugins under Settings in the Network Admin
+
+== Screenshots ==
+
+1. Totals
+2. 
+3. tagged plugins
+
+== Frequently Asked Questions ==
+
+= How do I tag a plugin? =
+
+Upload a file to the plugin's directory in the format `tag-name.tag`. For example, you might add a file to this plugin's directory called `utility.tag`
+Note that this only supports one tag per plugin.
+
+== Upgrade Notice ==
+
+= 1.5 =
+Not pretty, but not as ugly either. Code cleanup.
+
+= 1.5 =
+A little prettier.
+
+= 1.4 =
+plugin now shows inactive plugins (plugins with 0 users).
+
+== Changelog ==
+
+= 1.6 =
+* Verified agains 3.5
+* More code clean up
+* Slight de-uglification.
+
+= 1.5 =
+* Finally updated for 3.2 (and 3.3-beta3 while we're at it)
+* Cleaned up code
+* A little bit prettier. Not that it's pretty, just "er"
+
+= 1.4.1 =
+* forgot to remove that part that said this plugin doesn't list inactive plugins
+
+= 1.4 =
+* now shows inactive plugins
+
+= 1.3 =
+* sorts the 'totals' list  alphabetically
+* adds tagged sorting
+
+= 1.2 =
+* adds link to each site's plugin page for convenience
+
+= 1.1 =
+* quick fix to use table prefix variable instead of assuming default
+
+= 1.0 =
+* Initial Release.
